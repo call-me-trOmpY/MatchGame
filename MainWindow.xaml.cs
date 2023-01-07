e59d0808380b1c37eb5996ed957d4ea92d95fa2f
call-me-trOmpY <86676566+call-me-trOmpY@users.noreply.github.com>
2023-01-07 15:48:23 -0600

    done, some improvement can be done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MatchGame
 {
@@ -20,10 +22,33 @@ namespace MatchGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+      
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
+
             SetUpGame();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text += " - Play again?";
+            }
         }
 
         private void SetUpGame()
@@ -38,10 +63,51 @@ namespace MatchGame
 
             foreach (TextBlock textBlock in mainGrid.Children.OfType <TextBlock>())     //Find every TextBock in the main grid and repeat the statement for each of them
             {
-                int index = random.Next(animalEmoji.Count);                             //Picks a random number between 0 and the number of emoji left in the list and call it "index"
-                string nextEmoji = animalEmoji[index];                                  //Use the random number called "index" to get a random emoji from the list
-                textBlock.Text = nextEmoji;                                             //Update the TextBlock with the random emoji from the list
-                animalEmoji.RemoveAt(index);                                            //Remove the random emoji from the list
+                if (textBlock.Name != "timeTextBlock")
+                {
+                    textBlock.Visibility = Visibility.Visible;
+                    int index = random.Next(animalEmoji.Count);                             //Picks a random number between 0 and the number of emoji left in the list and call it "index"
+                    string nextEmoji = animalEmoji[index];                                  //Use the random number called "index" to get a random emoji from the list
+                    textBlock.Text = nextEmoji;                                             //Update the TextBlock with the random emoji from the list
+                    animalEmoji.RemoveAt(index);                                             //Remove the random emoji from the list
+                }
+            }
+
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
+        }
+
+        private void textBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+
+
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                matchesFound++;
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
+
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //SetUpGame();
+            if (matchesFound == 8)
+            {
+                SetUpGame();
             }
         }
     }
